c7db34a8ec6085a6b602bbb69b5c17583b1a53d9
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2021-09-19 14:18:50 +0530

    Update sign_number.sh

--- assets/sign_number.sh
+++ assets/sign_number.sh
@@ -1,12 +1,40 @@
-echo "Enter a Number"
-read num
+echo "Enter 10 numbers:"
+declare -a a
+for((i=0;i<10;i++))
+do
+  read a[$i]
+done
+for i in ${a[@]}
+do
+   if [ $i == 0 ]
+   then
+         ((c0=$c0 + 1))
+   elif [ $i -le 0 ]
+   then
+         ((cn=$cn + 1))
+   else
+         ((cp=$cp + 1))            
+   fi
+done
+echo "Positive numbers: $cp"        
+echo "No. of zero's number: $c0"  
+echo "Negative numbers: $cn" 
 
-if [ $num -lt 0 ]
-then
-    echo "Negative"
-elif [ $num -gt 0 ]
-then
-    echo "Positive"
-else
-    echo "Zero"
-fi
+
+for((i=0;i<9;i++))
+do
+  for((j=i+1;j<10;j++))
+  do
+     if((a[i]>a[j]))
+     then
+     temp=${a[i]}
+     a[$i]=${a[j]}
+     a[$j]=$temp
+     fi  
+  done
+done
+echo "sorted order: "
+for((i=0;i<10;i++))
+do
+  echo ${a[i]}
+done
